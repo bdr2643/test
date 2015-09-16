5a946b4904af25346cc459f2899b5338c7479f37
unknown <bdr2643@AN112502-CT103.nvstu.edu>
2015-09-16 14:36:36 -0400

    adding files

---
+++ Rectangle2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Rectangle2 {
+        public static void main(String[]args) {
+	                Scanner keyboard = new Scanner(System.in);
+			                double h, w, a, p;
+					                System.out.println("Enter width");
+							                w = keyboard.nextDouble();
+									                System.out.println("Ender height");
+											                h = keyboard.nextDouble();
+													                p = 2*(h+w);
+															                a = h*w;
+																	                System.out.println("The perimeter is " + p);
+																			                System.out.println("The area is " + a );
+																					                }
+																							        }
+
